ead5908ae650f78169f21bf55f47aa5463f50230
gorcajo <guillermo.orcajo@hoplasoftware.com>
2022-02-28 16:28:55 +0100

    envinspector app

---
+++ envinspector/main.py
@@ -0,0 +1,48 @@
+import json
+import logging
+import os
+
+from flask import Flask, Response, request
+from werkzeug.exceptions import NotFound, MethodNotAllowed
+
+
+logging.basicConfig(
+    format  = "%(asctime)-5s.%(msecs)03d | %(levelname)-7s | %(threadName)10s | %(module)s:L%(lineno)d | %(message)s",
+    level   = logging.DEBUG,
+    datefmt = "%Y-%m-%d %H:%M:%S")
+
+logging.getLogger('werkzeug').setLevel(logging.ERROR)
+
+
+app = Flask(__name__)
+
+
+@app.before_request
+def before_request() -> Response:
+    logging.info(f'Request "{request.method.upper()} {request.path.lower()}" from "{request.remote_addr}"')
+
+
+@app.after_request
+def after_request(response: Response) -> Response:
+    logging.info(f'Returning HTTP {response.status_code}')
+    return response
+
+
+@app.errorhandler(Exception)
+def handle_exceptions(e) -> Response:
+    if isinstance(e, NotFound):
+        logging.error('Returning HTTP 404')
+        return Response(status=404, response=json.dumps({'error': 'HTTP 404 Not Found'}), content_type='application/json')
+    elif isinstance(e, MethodNotAllowed):
+        logging.error('Returning HTTP 405')
+        return Response(status=405, response=json.dumps({'error': 'HTTP 405 Method Not Allowed'}), content_type='application/json')
+    else:
+        logging.error('Exception!', e)
+        return Response(status=500, response=json.dumps({'error': 'HTTP 500 Internal Server Error'}), content_type='application/json')
+
+
+@app.route('/envvars', methods=['GET'])
+def get_environment_variables() -> Response:
+    keys = list(os.environ.keys())
+    envvars = {key: os.environ[key] for key in keys}
+    return Response(status=200, response=json.dumps(envvars, sort_keys=True), content_type='application/json')
